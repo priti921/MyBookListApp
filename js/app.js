23c96ac1571dd5471c27b6cab63d4905c2ff7494
Priti <roypriti921@gmail.com>
2019-10-10 18:18:24 +0530

    added form validation

--- js/app.js
+++ js/app.js
@@ -52,9 +52,21 @@ class UI {
         }
     }
 
-    // static showAlert(message, className) {
-    //     const div
-    // }
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+
+        container.insertBefore(div, form);
+        // vanish in 3 seconds
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
 
     static clearFields(){
         document.querySelector('#title').value = '';
@@ -76,8 +88,10 @@ document.querySelector('#book-form').addEventListener('submit', e => {
 
     //Validate
     if(title === '' || author === '' || isbn === ''){
-        alert('please fill in all the fields');
+        UI.showAlert('Please fill in all the fields', 'danger');
     }else{
+        //show success
+        UI.showAlert('You added a book!', 'success');
         //instatiate book
     const book = new Book (title, author, isbn);
 
@@ -92,4 +106,7 @@ document.querySelector('#book-form').addEventListener('submit', e => {
 //Event: remove a book
 document.querySelector('#book-list').addEventListener('click', e => {
     UI.deleteBook(e.target);
+
+    //show remove alert
+    UI.showAlert('Book removed!', 'danger');
 })
